c93aee55f78e36d04276a682042d30f83f2368d8
Jordan Hill <jormhill@gmail.com>
2014-01-18 20:47:44 -0800

    Built a simple DNA permutation generator.

--- scripts/metrics.js
+++ scripts/metrics.js
@@ -1,12 +1,35 @@
 var Metrics = (function() {
     
-    var permuteDNA;
+    var _permuteByRadix;
     
-    permuteDNA = function(size) {
-        var bps = ['A','G','T','C'];
+    _permuteByRadix = function(size) {
+        var bps = ['A','G','T','C'],
+            perms = Math.pow(4, size),
+            str;
         
+        // maybe not the best approach, but it does the job
+        for(var i=0; i < perms; i++) {
+            str = i.toString(4)
+                .replace(/0/g, 'A')
+                .replace(/1/g, 'G')
+                .replace(/2/g, 'T')
+                .replace(/3/g, 'C');
+                
+            while(temp.length < size) {
+                str = 'A' + str;
+            }
+            
+            console.log(temp);
+        }
         
     };
+    
+    return {
+        permuteDNA: function(size) {
+            return _permuteByRadix(size);
+        }
+    };
+    
     
     
     // old stuff
